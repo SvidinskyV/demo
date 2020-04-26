f2f05a1eee542e7988e7e5e8866952a131b1f1ef
midav6893 <x-lorex@mail.ru>
2020-04-27 01:24:53 +0300

    add task entity

---
+++ src/main/java/com/example/demo/Task.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+public class Task {
+
+    private Long id;
+    private String subject;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+}
